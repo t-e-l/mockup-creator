a438c481b1873838c2101c5cf9486e9f15c088f5
v1nc <github@reckendrees.systems>
2021-01-07 19:03:21 +0100

    use tmp instead of /tmp

--- create.py
+++ create.py
@@ -18,6 +18,8 @@ def add_corners (im, rad):
     return im
 
 
+os.system("mkdir tmp")
+
 #positions to paste screen and phone
 SCREEN_POS = (257,295)
 PHONE_POS = (248,270)
@@ -34,8 +36,8 @@ if not ".gif" in image_name:
 	print("=> converting your input to gif first")
 	#scaling to 780 width to fit template
 	#for other heights adjust cutout value
-	os.system('ffmpeg -i {} -vf "fps=10,scale=780:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 /tmp/tmp.gif -y'.format(image_name))
-	image_name = "/tmp/tmp.gif"
+	os.system('ffmpeg -i {} -vf "fps=10,scale=780:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 tmp/tmp.gif -y'.format(image_name))
+	image_name = "tmp/tmp.gif"
 
 #open images
 background = Image.open("bg.png")
@@ -92,13 +94,13 @@ for frame in ImageSequence.Iterator(im):
 	frames.append(new_frame)
 
 #save gif		
-frames[0].save("/tmp/tmp2.gif", save_all=True, append_images=frames[1:],loop=0)
+frames[0].save("tmp/tmp2.gif", save_all=True, append_images=frames[1:],loop=0)
 
 #save webm and mp4
-os.system("ffmpeg -i /tmp/tmp2.gif -an -c vp9 -b:v 0 -crf 20 -pass 1 -f webm /dev/null -y")
-os.system("ffmpeg -i /tmp/tmp2.gif -an -c vp9 -b:v 0 -crf 20 -pass 2 {} -y".format(output_webm))
-os.system("ffmpeg -i {} -c copy /tmp/tmp.mp4 -y".format(output_webm,output_mp4))
+os.system("ffmpeg -i tmp/tmp2.gif -an -c vp9 -b:v 0 -crf 20 -pass 1 -f webm /dev/null -y")
+os.system("ffmpeg -i tmp/tmp2.gif -an -c vp9 -b:v 0 -crf 20 -pass 2 {} -y".format(output_webm))
+os.system("ffmpeg -i {} -c copy tmp/tmp.mp4 -y".format(output_webm,output_mp4))
 
 #extract first frame and set thumbnail
-os.system('ffmpeg -i /tmp/tmp2.gif -vf "select=eq(n\,0)" -q:v 3 /tmp/thumbnail.png -y')
-os.system('ffmpeg -i /tmp/tmp.mp4 -i /tmp/thumbnail.png -map 1 -map 0 -c copy -disposition:0 attached_pic {0} -y '.format(output_mp4))
+os.system('ffmpeg -i tmp/tmp2.gif -vf "select=eq(n\,0)" -q:v 3 tmp/thumbnail.png -y')
+os.system('ffmpeg -i tmp/tmp.mp4 -i tmp/thumbnail.png -map 1 -map 0 -c copy -disposition:0 attached_pic {0} -y '.format(output_mp4))
